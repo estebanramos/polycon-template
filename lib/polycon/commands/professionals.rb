4b765e5c4bbf5a8956566051e1b938b3ff2e544f
Esteban Ramos <estebanramos438@gmail.com>
2021-10-02 00:13:36 -0300

    Primer Commit. Funcionalidades basicas de Professsional

--- lib/polycon/commands/professionals.rb
+++ lib/polycon/commands/professionals.rb
@@ -1,7 +1,14 @@
 module Polycon
   module Commands
     module Professionals
+      def get_professional_format(name)
+        professional_name = name.split(" ")
+        directory_name = professional_name[0]+'-'+professional_name[1]
+        return directory_name
+      end
+      
       class Create < Dry::CLI::Command
+        include Professionals
         desc 'Create a professional'
 
         argument :name, required: true, desc: 'Full name of the professional'
@@ -12,7 +19,13 @@ module Polycon
         ]
 
         def call(name:, **)
-          warn "TODO: Implementar creación de un o una profesional con nombre '#{name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          #warn "TODO: Implementar creación de un o una profesional con nombre '#{name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          begin
+            FileUtils.mkdir ".polycon/#{get_professional_format(name)}" #unless Dir.entries('.polycon/'+directory_name)
+          rescue Errno::EEXIST => exception 
+            warn 'ERROR: Ya existe un profesional con ese nombre'
+          end
+
         end
       end
 
@@ -39,11 +52,25 @@ module Polycon
         ]
 
         def call(*)
-          warn "TODO: Implementar listado de profesionales.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          ##warn "TODO: Implementar listado de profesionales.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          begin
+            directorios = Dir.entries("./.polycon")
+            # Borro el . y ..
+            directorios.delete(".")
+            directorios.delete("..")
+            warn "[#] Profesionales [#]"
+            directorios.each do |d|
+              d = d.split("-")
+             warn "#{d[0]+' '+d[1]}"
+            end
+          rescue => exception
+            warn exception
+          end
         end
       end
 
       class Rename < Dry::CLI::Command
+        include Professionals
         desc 'Rename a professional'
 
         argument :old_name, required: true, desc: 'Current name of the professional'
@@ -54,7 +81,14 @@ module Polycon
         ]
 
         def call(old_name:, new_name:, **)
-          warn "TODO: Implementar renombrado de profesionales con nombre '#{old_name}' para que pase a llamarse '#{new_name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          #warn "TODO: Implementar renombrado de profesionales con nombre '#{old_name}' para que pase a llamarse '#{new_name}'.\nPodés comenzar a hacerlo en #{__FILE__}:#{__LINE__}."
+          old_name = get_professional_format(old_name)
+          new_name = get_professional_format(new_name)
+          begin
+          FileUtils.mv ".polycon/#{old_name}", ".polycon/#{new_name}"
+          rescue => exception
+            warn "ERROR: Ha surgido un problema renombrando al Profesional"
+          end
         end
       end
     end
